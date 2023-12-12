a4bf783ff4226eee24fa21722c97c44adfb719f6
Siripoom <artorsiriratpoom@gmail.com>
2023-12-12 21:43:23 +0700

    commit

---
+++ controllers/storeUserController.js
@@ -0,0 +1,13 @@
+const User = require('../models/User')
+
+module.exports = (req,res)=>{
+    User.create(req.body).then(()=>{
+        console.log("User registered successfully")
+    }).catch(error=>{
+        // console.log(error)
+        if(error){
+            const
+        }
+        
+    })
+}
--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const mongoose = require('mongoose')
-const expressSession = require('express-session')
-const flash = require('connect-flash')
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+// MongoDB Connection
+mongoose.connect(
+  "mongodb+srv://root:root1234@cluster0.5lquezy.mongodb.net/?retryWrites=true&w=majority"
+//   {
+//     useNewUrlParser: true,
+//   }
+);
 
 //Controllers
-const indexController = require("./controllers/indexController")
+const indexController = require("./controllers/indexController");
+const loginController = require("./controllers/loginController");
+const registerController = require("./controllers/registerController");
+const storeUserController = require("./controllers/storeUserController");
 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(flash());
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(flash())
-app.set('view engine', 'ejs')
+app.get("/", indexController);
+app.get("/login", loginController);
+app.get("/register", registerController);
+app.post('/user/register',storeUserController)
 
-app.get('/',indexController)
-
-app.listen(4000,()=>{
-    console.log('App listening on port 4000')
-})
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
+});
