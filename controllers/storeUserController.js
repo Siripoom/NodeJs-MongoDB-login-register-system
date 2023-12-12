24345ed16487d366bb92b2ffb39ce2ace79c2251
Siripoom <artorsiriratpoom@gmail.com>
2023-12-12 21:44:09 +0700

    commit

--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -4,10 +4,10 @@ module.exports = (req,res)=>{
     User.create(req.body).then(()=>{
         console.log("User registered successfully")
     }).catch(error=>{
-        // console.log(error)
-        if(error){
-            const
-        }
+        console.log(error)
+        // if(error){
+        //     const validationErrors 0
+        // }
         
     })
 }
